b8cb1a1b141c20d2e2759156d77d790b64eefef6
Dhruvv2003 <chanddhruv00@gmail.com>
2024-12-25 16:31:18 +0530

    commit for merge repo of client & server

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/home"
+import SignIn from "./pages/signin"
+import SignUp from "./pages/signup"
+import About from "./pages/about"
+import Profile from "./pages/profile"
+import Header from "./components/header.jsx";
+export default function App() {
+  return <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path="/" exact element={<Home />} />
+    <Route path="/Sign-Up" exact element={<SignUp />} />
+    <Route path="/Sign-In" exact element={<SignIn />} />
+    <Route path="/Profile" exact element={<Profile />} />
+    <Route path="/About" exact element={<About />} />
+    </Routes>
+    </BrowserRouter>
+};
+
---
+++ client/src/pages/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div class="text-center bg-blue-500 p-4">
+     Hello, Tailwind CSS!
+    </div>
+  )
+}
