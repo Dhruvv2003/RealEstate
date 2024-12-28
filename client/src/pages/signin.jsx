29765da69ae7950440466e6e28d13c5c9daaa1b6
Dhruvv2003 <chanddhruv00@gmail.com>
2024-12-29 02:24:23 +0530

    nothing

--- client/src/pages/signin.jsx
+++ client/src/pages/signin.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice.js';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -36,6 +36,7 @@ export default function SignIn() {
       navigate('/');
     }
     catch(error){
+      console.log(signInFailure);
       dispatch(signInFailure(error.message));
     }
     
