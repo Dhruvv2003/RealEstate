1db8df97d7b6d82f1b5d6a0adac1cd479a549750
Dhruvv2003 <chanddhruv00@gmail.com>
2025-01-08 00:07:40 +0530

    error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/home.jsx';
 import SignIn from './pages/signin.jsx';
 import SignUp from './pages/signup.jsx';
 import About from './pages/about.jsx';
-import Profile from './pages/Profile';
+import Profile from './pages/profile.jsx';
 import Header from './components/header.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
 import CreateListing from './pages/CreateListing.jsx';
-import Listing from './pages/Listing';
-import Search from './pages/Search';
+import Listing from './pages/Listing.jsx';
+import Search from './pages/Search.jsx';
 
 
 
